e5e4e108d9e529c7e39508dd5bc81eb74f4a7762
Joni Halabi <jh1955@georgetown.edu>
2016-06-30 11:01:14 -0400

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,73 @@
+var doc = {
+  init: function() {
+    var _this = this;
+
+    $('#generate').click(function(e) { 
+      e.preventDefault();
+      _this.generate();
+    });
+
+    $('#copy').click(function(e) { 
+      e.preventDefault();
+      _this.copy();
+    });
+
+    $('#clear').click(function(e) { 
+      e.preventDefault();
+      _this.clear();
+    });
+
+    $('#options a').click(function(e) { 
+      e.preventDefault();
+      _this.paste(this);
+    });
+  },
+
+  /* ---
+   * FUNCTION: generate()
+   *    When the code form is submitted by the user, copy any code in the field to
+   *    the output block so that the user can see what that code will look like.
+   */
+  generate: function() {
+    var code = $('#code').val();
+    $('#output').html(code);
+  },
+
+  /* ---
+   * FUNCTION: copy()
+   *    When a user clicks on the copy button, the contents of the field are
+   *    copied to the clipboard.
+   */
+  copy: function() {
+    $('#code').select();
+    document.execCommand('copy');
+    document.getSelection().removeAllRanges();
+  },
+
+  /* ---
+   * FUNCTION: clear()
+   *    When a user clicks on the "start over" button, the contents of the field deleted
+   */
+  clear: function() {
+    $('#code').val('');
+    this.generate();
+  },
+
+  /* ---
+   * FUNCTION: paste()
+   *    When a user clicks on a code option, the HTML associated with that option
+   *    is pasted into the field.
+   */
+  paste: function(el) {
+    var id = $(el).attr('data-option');
+    var html = $('#' + id).html();
+
+    var currentCode = $('#code').val();
+    $('#code').val(currentCode + html);
+    this.generate();
+  }
+};
+
+$(document).ready(function() {
+  doc.init();
+});
